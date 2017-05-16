d6d596de80ea68c89bdd2c1229330ca661e91228
虎振东 <huzd@primeton.com>
2017-05-16 14:11:14 +0800

    配置

--- src/main/java/com/primeton/sample/SampleApplication.java
+++ src/main/java/com/primeton/sample/SampleApplication.java
@@ -2,16 +2,25 @@ package com.primeton.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author <a href='mailto:huzd@primeton.com'>虎振东</a>
  * @date 2017/5/16 09:56.
  */
+@RestController
 @SpringBootApplication
 public class SampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SampleApplication.class,args);
+        SpringApplication.run(SampleApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public Object hello(String name) {
+        return String.format("Hello %s !", name);
     }
 
 }
