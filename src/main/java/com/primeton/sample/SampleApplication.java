4f802f665a0f6aae1eb172e8393c98d5bbfef8c3
虎振东 <huzd@primeton.com>
2017-05-16 10:07:15 +0800

    添加ignore文件

---
+++ src/main/java/com/primeton/sample/SampleApplication.java
@@ -0,0 +1,17 @@
+package com.primeton.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href='mailto:huzd@primeton.com'>虎振东</a>
+ * @date 2017/5/16 09:56.
+ */
+@SpringBootApplication
+public class SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApplication.class,args);
+    }
+
+}
